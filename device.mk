e3e8c8bdd0b242747c5e4c6da68ccb6a89768669
CrickFoma <arikdavydov1934@gmail.com>
2026-04-27 19:23:38 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -106,3 +106,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
+
+# Копирование HAL-сервисов дешифровки в ramdisk
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/recovery/root/vendor/bin/hw/android.hardware.gatekeeper@1.0-service:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/bin/hw/android.hardware.gatekeeper@1.0-service \
+    $(DEVICE_PATH)/recovery/root/vendor/bin/hw/android.hardware.keymaster@4.1-service.trustonic:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/bin/hw/android.hardware.keymaster@4.1-service.trustonic \
+    $(DEVICE_PATH)/recovery/root/vendor/bin/hw/vendor.trustonic.tee@1.1-service:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/bin/hw/vendor.trustonic.tee@1.1-service
